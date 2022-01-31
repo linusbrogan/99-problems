b5fb03e1fef15079a911ecf8d47257ae82c94eba
Linus Brogan <linusbrogan@lclark.edu>
2022-01-31 12:08:00 -0800

    Wrap rnd_select in IO monad

--- P23.hs
+++ P23.hs
@@ -3,10 +3,12 @@ module P23
     ) where
 
 run :: IO ()
-run = do { print $ rnd_select "abcdefgh" 3
+run = do { list <- rnd_select "abcdefgh" 3
+         ; print list
          }
 
-rnd_select :: [a] -> Int -> [a]
-rnd_select [] _ = []
-rnd_select _ 0 = []
-rnd_select (x:xs) n = x:(rnd_select xs (n - 1))
+rnd_select :: [a] -> Int -> IO [a]
+rnd_select [] _ = do return []
+rnd_select _ 0 = do return []
+rnd_select (x:xs) n = do list <- (rnd_select xs (n - 1))
+                         return (x:list)
