e2bd52cb96fa3f88206c8d5d90026a4bcb5c65c8
Linus Brogan <git@linusbrogan.com>
2022-09-05 17:10:46 -0700

    Work on Problem 23

--- P23.hs
+++ P23.hs
@@ -2,6 +2,8 @@ module P23
     ( run
     ) where
 
+import System.Random
+
 run :: IO ()
 run = do { list <- rnd_select "abcdefgh" 3
          ; print list
@@ -10,5 +12,7 @@ run = do { list <- rnd_select "abcdefgh" 3
 rnd_select :: [a] -> Int -> IO [a]
 rnd_select [] _ = do return []
 rnd_select _ 0 = do return []
-rnd_select (x:xs) n = do list <- (rnd_select xs (n - 1))
+rnd_select (x:xs) n = do g <- newStdGen
+                         let (i, _) = randomR (1, length list) g
+                         list <- (rnd_select xs (n - 1))
                          return (x:list)
